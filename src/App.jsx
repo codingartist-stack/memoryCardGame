b59c5961f862fdca6f2a7b93aad1db701c982bb1
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-16 16:01:04 -0800

    needed useEffect to get fetch the pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-const cards = ['A', 'B', 'C', 'D', 'E', 'F'];
-
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const GenOne = 151;
+  const randomNumber = (generation) => {
+    return Math.floor(Math.random() * generation);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${randomNumber(GenOne)}`
+      );
+
+      result.json().then((json) => {
+        console.log(json);
+      });
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <h1>Memory Card Game</h1>
---
+++ src/getPokemon.jsx
@@ -0,0 +1,3 @@
+//get random number generation I pokemon 1 - 151
+//get pokemon
+//get pokemon image
