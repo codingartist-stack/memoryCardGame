493c607a260f6c89553c8dfb8f188ee259c58f8a
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-17 13:32:34 -0800

    just working on the layout of the page because I wanted to avoid api for a little bit.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,30 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import CreateBoard from './components/createBoard';
 
+const testingDeck = [
+  { id: 1, name: 'blue' },
+  { id: 2, name: 'red' },
+  { id: 3, name: 'yellow' },
+];
+
 const App = () => {
-  const [deck, setDeck] = useState(['blue', 'red', 'yellow']);
+  const [deck, setDeck] = useState([]);
   const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledDeck = [...testingDeck, ...testingDeck].sort(
+      () => Math.random() - 0.5
+    );
+    console.log(shuffledDeck);
+
+    setDeck(shuffledDeck);
+    setTurns(0);
+  };
 
   return (
     <>
       <h1>Memory Card Game</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
       <CreateBoard deck={deck} />
     </>
   );
--- src/components/createBoard.jsx
+++ src/components/createBoard.jsx
@@ -1,15 +1,17 @@
 export default function CreateBoard(props) {
   if (props.deck.length === 0) {
-    return;
+    return <h2>Click New Game to start</h2>;
   } else {
-    <main className="gameBoard">
-      {props.deck.map((card) => {
-        return (
-          <div className="card" key={props.deck.indexOf(card)}>
-            Hello
-          </div>
-        );
-      })}
-    </main>;
+    return (
+      <main className="gameBoard">
+        {props.deck.map((card) => {
+          return (
+            <div className="card" id={card.id} key={crypto.randomUUID()}>
+              {card.name}
+            </div>
+          );
+        })}
+      </main>
+    );
   }
 }
