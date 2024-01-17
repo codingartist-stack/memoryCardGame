f820c1d2d2ede279e3c538f740570f2e4e99958a
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-17 11:51:04 -0800

    ben tried to figure out why is was not working and tried a lot of things and it still didn't work. the code did not look the same after Ben tried to work on it. it was completely different. He said I should start from scratch.... so I did

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  const [cards, setCards] = useState([]);
-  const [turns, setTurns] = useState(0);
-
-  const GenOne = 151;
-  const usedNumbers = new Set();
-
-  const getRandomNumber = (generation) => {
-    let randomNumber = Math.floor(Math.random() * generation) + 1;
-
-    while (usedNumbers.has(randomNumber)) {
-      randomNumber = Math.floor(Math.random() * generation) + 1;
-    }
-    usedNumbers.add(randomNumber);
-
-    return randomNumber;
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      for (let i = 0; i < 5; i++) {
-        const response = await fetch(
-          `https://pokeapi.co/api/v2/pokemon/${getRandomNumber(GenOne)}`,
-          { mode: 'cors' }
-        );
-
-        const poke = await response.json();
-
-        const pokemonID = poke.id;
-        const pokemonName = poke.name;
-        const pokemonURL = poke.sprites.front_default;
-
-        setCards((currentCards) => {
-          return [
-            ...currentCards,
-            { id: pokemonID, name: pokemonName, url: pokemonURL },
-            { id: pokemonID, name: pokemonName, url: pokemonURL },
-          ];
-        });
-      }
-    };
-
-    fetchData();
-    console.log(cards);
-  }, []);
-
+const App = () => {
   return (
     <>
       <h1>Memory Card Game</h1>
-
       <button>New Game</button>
     </>
   );
-}
+};
 
 export default App;
