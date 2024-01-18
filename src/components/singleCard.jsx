fe50a9bfed0e805e6c0f9b1db459246ddc229890
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-18 15:02:57 -0800

    had willow try out the game. I moved the onclick event to the card it's self. the animation is still not working. I am not sure why.

--- src/components/singleCard.jsx
+++ src/components/singleCard.jsx
@@ -6,13 +6,16 @@ export default function SingleCard({ card, handleChoice, flipped, disabled }) {
   };
 
   return (
-    <div className={flipped ? 'flipped card' : 'card'} id={card.id}>
+    <div
+      className={flipped ? 'flipped card' : 'card'}
+      id={card.id}
+      onClick={handleClick}
+    >
       <img src={card.url} alt={card.name} className="front" />
       <img
         src="./src/assets/img/coverPokeball.png"
         alt="pokeBall"
         className="back"
-        onClick={handleClick}
       />
     </div>
   );
