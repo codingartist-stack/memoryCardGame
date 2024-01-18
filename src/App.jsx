0f87fbcc469096bf210d89d509fa4d8274f1143f
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-17 18:49:29 -0800

    the shuffle function works on the console log but is not rendering the changes. Ben could not figure it out. looks like I will on it tomorrow.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { useAsync } from 'react-use';
+import { useState, useEffect, setValue } from 'react';
+import { useEffectOnce } from 'react-use';
 
 import './App.css';
 import Board from './components/createBoard';
@@ -12,60 +12,60 @@ const App = () => {
   const usedNumbers = new Set();
 
   const getRandomNumber = (generation) => {
-    let number = Math.floor(Math.random() * 151) + 1;
+    let number = Math.floor(Math.random() * generation) + 1;
 
     while (usedNumbers.has(number)) {
-      number = Math.floor(Math.random() * 151) + 1;
+      number = Math.floor(Math.random() * generation) + 1;
     }
 
     usedNumbers.add(number);
     return number;
   };
+  const [deck, setDeck] = useState([]);
 
-  const {
-    value: deck,
-    loading,
-    error,
-  } = useAsync(async () => {
-    // loop
-    const deck = [];
-    for (let i = 0; i < 5; i++) {
-      const response = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${getRandomNumber(genOne)}/`
-      );
-      const pokemon = await response.json();
+  useEffect(() => setDeck(deck));
 
-      const pokemonID = pokemon.id;
-      const pokemonName = pokemon.name;
-      const pokemonURL = pokemon.sprites.front_default;
+  useEffectOnce(() => {
+    const getPokemonCards = async () => {
+      // loop
+      let deck = [];
+      for (let i = 0; i < 6; i++) {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${getRandomNumber(genOne)}/`
+        );
+        const pokemon = await response.json();
 
-      deck.push({ id: pokemonID, name: pokemonName, url: pokemonURL });
-    }
-    return deck;
+        const pokemonID = pokemon.id;
+        const pokemonName = pokemon.name;
+        const pokemonURL = pokemon.sprites.front_default;
+
+        deck.push(
+          { id: pokemonID, name: pokemonName, url: pokemonURL },
+          { id: pokemonID, name: pokemonName, url: pokemonURL }
+        );
+      }
+      deck = deck.sort(() => Math.random() - 0.5);
+      setDeck(deck);
+    };
+    getPokemonCards();
   });
 
-  // const shuffleCards = () => {
-  //   const shuffledDeck = [...deck, ...deck].sort(() => Math.random() - 0.5);
-  //   console.log(shuffledDeck);
+  const shuffleCards = () => {
+    // e.preventDefault();
+    let shuffledDeck = [];
+    shuffledDeck = deck.sort(() => Math.random() - 0.5);
 
-  //   setTurns(0);
-  //   return shuffledDeck;
-  // };
-
-  // console.log(deck);
+    console.log(shuffledDeck);
+    //   setTurns(0);
+    setDeck(shuffledDeck);
+  };
 
   return (
     <>
       <h1>Memory Card Game</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
       <main className="gameBoard">
-        {loading ? (
-          <div>Loading...</div>
-        ) : error ? (
-          <div>Error: {error.message}</div>
-        ) : (
-          <Board deck={deck} />
-        )}
+        <Board deck={deck} />
       </main>
     </>
   );
--- src/components/createBoard.jsx
+++ src/components/createBoard.jsx
@@ -1,10 +1,10 @@
-export default function Board(props) {
-  if (props.deck.length === 0) {
+export default function Board({ deck }) {
+  if (deck.length === 0) {
     return <h2>Click New Game to start</h2>;
   } else {
     return (
       <>
-        {props.deck.map((card) => {
+        {deck.map((card) => {
           return (
             <div className="card" id={card.id} key={crypto.randomUUID()}>
               <img src={card.url} alt={card.name} />
