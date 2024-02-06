63dffff4d21f65912888cf072e7c6af56a3ae049
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-02-05 20:42:59 -0800

    I couldn't figure out how to get the mouse tracking to work on a hover. I got to work if I just want to track it the whole time, but not on a hover.

--- src/components/singleCard.jsx
+++ src/components/singleCard.jsx
@@ -1,18 +1,5 @@
-// import pokeBall from './../assets/img/coverPokeball.png';
-import { useState } from "react";
-import useMousePosition from "../mouseTracking";
-
 
 export default function SingleCard({ card, handleChoice, flipped, disabled }) {
-  const mousePosition = useMousePosition();
-  const [hover, setHover] = useState(false)
-  // const [cardStyle, setCardStyle] = useState(`transform: rotate(${mousePosition.x}deg) rotateY(${mousePosition.x}deg) rotateX(${-1 * mousePosition.y}deg) perspective(23rem)`);
-
-  const toggleHover = () => {
-    setHover(!hover);
-  }
-
-
 
   const handleClick = () => {
     if (!disabled) {
@@ -20,16 +7,11 @@ export default function SingleCard({ card, handleChoice, flipped, disabled }) {
     }
   };
 
-
   return (
     <div
       className={flipped ? 'flipped card' : 'card'}
       id={card.id}
       onClick={handleClick}
-      style={{transform: `rotate(${mousePosition.x}deg) rotateY(${mousePosition.x}deg) rotateX(${-1 * mousePosition.y}deg) perspective(23rem)`}}
-      onMouseEnter={toggleHover}
-      onMouseLeave={toggleHover}
-      // style={{cardStyle}}
     >
       <img src={card.url} alt={card.name} className="front" />
       <div className="back">
